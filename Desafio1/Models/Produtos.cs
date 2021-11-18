6dba7a15acb0dbf94f5c8efc8abd4857d3ce87cf
Vinicius Souza <viniciusrmsouza577@gmail.com>
2021-11-17 21:35:56 -0300

    Criando o projeto, adicionando classe produtos e o contexto

---
+++ Desafio1/Models/Produtos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio1.Models
+{
+    public class Produtos
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public double ValorUnitario { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
---
+++ Desafio1/Models/TodoContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Desafio1.Models
+{
+    public class TodoContext : DbContext
+    {
+        public DbSet<Produtos> Produtos { get; set; }
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+            
+        }
+    }
+}
